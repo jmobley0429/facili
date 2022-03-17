67b09b5071ff18f7bc195736cd3def35bf7a274d
jmobley0429 <jmobley0429@gmail.com>
2022-03-17 17:37:07 -0500

    started styling scripts on create.html page

--- static/js/create.js
+++ static/js/create.js
@@ -1,8 +1,8 @@
 const formDivs = document.querySelectorAll("div.list-item.edit");
 const showDivs = document.querySelectorAll(".list-item");
 const editButton = document.querySelector("#editButton");
-const shareButton = document.querySelector("#shareutton");
-const editWarningSpan = document.querySelector("#editButton+span");
+const shareButton = document.querySelector("#shareButton");
+const editWarningSpan = document.querySelector("#editButton + span");
 const shareWarningSpan = document.querySelector("#shareButton+span");
 let selectedDisc;
 
@@ -17,16 +17,25 @@ showDivs.forEach((div, i) => {
     editButton.setAttribute("value", selectedDisc);
   });
 });
-
-editButton.addEventListener("click", function(e) {
-  let val = editButton.getAttribute("value");
+// edit button no item selected warning
+editButton.addEventListener("click", function() {
+  val = editButton.getAttribute("value");
   if (val === null) {
     editWarningSpan.classList.toggle("on");
-    editWarningSpan.classList.toggle("on");
-  } else {
-    let showDiv = document.querySelector(`div.list-item[value="${val}"]`);
-    let formDiv = document.querySelector(`div.list-item.edit[value="${val}"]`);
-    showDiv.classList.toggle("hidden");
-    formDiv.classList.toggle("hidden");
+    setTimeout(() => {
+      editWarningSpan.classList.toggle("on");
+    }, 2000);
+  }
+});
+
+// share button no item selected warning
+
+shareButton.addEventListener("click", function() {
+  val = editButton.getAttribute("value");
+  if (val === null) {
+    shareWarningSpan.classList.toggle("on");
+    setTimeout(() => {
+      shareWarningSpan.classList.toggle("on");
+    }, 2000);
   }
 });
