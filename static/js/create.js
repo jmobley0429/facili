1dec06ae668339c6ada594cd8d66ed696e7dfd8a
jmobley0429 <jmobley0429@gmail.com>
2022-03-18 09:33:11 -0500

    continued scripting of interactive elements of create page

--- static/js/create.js
+++ static/js/create.js
@@ -13,16 +13,28 @@ formDivs.forEach((item, i) => {
 });
 
 showDivs.forEach((div, i) => {
-  div.addEventListener("click", function(e) {
-    div.classList.toggle("selected");
-    selectedDisc = div.getAttribute("value");
-    editButton.setAttribute("value", selectedDisc);
-  });
+  div.addEventListener("click", selectDiv);
 });
+
+function selectDiv(e) {
+  let div = e.currentTarget;
+  var isSelected = div.classList.contains("selected");
+  showDivs.forEach((item, i) => {
+    item.classList.remove("selected");
+  });
+  if (isSelected) {
+    div.classList.remove("selected");
+  } else {
+    div.classList.add("selected");
+  }
+  selectedDisc = div.getAttribute("value");
+  editButton.setAttribute("value", selectedDisc);
+}
 
 // edit button no item selected warning
 shareButton.addEventListener("click", toggleWarning);
 editButton.addEventListener("click", toggleWarning);
+editButton.addEventListener("click", editDiscusssion);
 
 function toggleWarning(e) {
   msg = e.currentTarget.getAttribute("id");
@@ -38,14 +50,43 @@ function toggleWarning(e) {
   }
 }
 
+function editDiscusssion(e) {
+  var selector = `div.list-item[value="${selectedDisc}"]`;
+  var formSelector = `div.list-item.edit[value="${selectedDisc}"]`;
+  var titleInpSel = `${formSelector} input[name="title"]`;
+  var descInpSel = `${formSelector} textarea[name="description"]`;
+  var titleInput = document.querySelector(titleInpSel);
+  var descInput = document.querySelector(descInpSel);
+  var title = document.querySelector(`${selector} h3`).textContent;
+  var desc = document.querySelector(`${selector} p`).textContent;
+  var showDiv = document.querySelector(selector);
+  var formDiv = document.querySelector(formSelector);
+
+  console.log("hidden");
+  showDiv.classList.toggle("hidden");
+  showDiv.classList.toggle("selected");
+  titleInput.value = title;
+  descInput.value = desc;
+  formDiv.classList.toggle("hidden");
+  formDiv.classList.toggle("selected");
+}
+
+function onInput() {
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
+}
 //ham menu button
 let sideNav = document.querySelector("nav");
-hamMenu.addEventListener("click", function(e) {
-  sideNav.classList.toggle("closed");
-  document.addEventListener("click", e => {
-    console.log(sideNav.contains(e.target));
-    if (!e.target.id == "sideNav" && !sideNav.contains(e.target)) {
-      sideNav.classList.toggle("closed");
-    }
-  });
+const html = document.querySelector("html");
+
+hamMenu.addEventListener("click", toggleNav);
+sideNav.addEventListener("click", e => {
+  e.stopPropagation();
 });
+function toggleNav(e) {
+  e.stopPropagation();
+  sideNav.classList.toggle("open");
+  html.addEventListener("click", e => {
+    sideNav.classList.remove("open");
+  });
+}
